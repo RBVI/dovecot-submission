ad3000308484649eea82730df92fcf2b090ae47c
Greg Couch <gregc@cgl.ucsf.edu>
2024-07-15 00:04:08 -0700

    Add running commentary to zone creation

--- dovecot-zone.sh
+++ dovecot-zone.sh
@@ -9,10 +9,16 @@ IPSET=$ZONE
 PORT=9999/tcp
 
 FWC='firewall-cmd --permanent'
-$FWC --delete-zone=$ZONE
-$FWC --delete-ipset=$ZONE
+#echo "Removing $ZONE zone and $ZONE ipset if already there"
+#$FWC --quiet --delete-zone=$ZONE
+#$FWC --quiet --delete-ipset=$ZONE
+echo "Creating $ZONE zone"
 $FWC --new-zone=$ZONE
+echo "Creating $IPSET ipset"
 $FWC --new-ipset=$IPSET --type=hash:ip
+echo "Adding submission port $PORT to $ZONE zone"
 $FWC --zone=$ZONE --add-port=$PORT
+echo "Adding source $IPSET ipset to $ZONE zone"
 $FWC --zone=$ZONE --add-source=ipset:$IPSET
+echo "Reloading firewall zone configuration"
 firewall-cmd --reload
